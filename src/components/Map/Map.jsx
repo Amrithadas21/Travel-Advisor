0a42a0ec0414680c79381d49cf54fbdb33be3eaf
Amrithadas21 <amrithadas21@gmail.com>
2025-06-22 16:48:03 +0530

    Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+
+          import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 import useStyles from './style';
+import mapStyles from './mapStyles';
 const Map = ({setCoordinates,setBounds,coordinates,places,setChildClicked}) => {
     
     const classes =useStyles();
@@ -16,12 +18,13 @@ const Map = ({setCoordinates,setBounds,coordinates,places,setChildClicked}) => {
        
             <div className={classes.mapContainer}>
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyDLYuIr5zAJP1IZ0wOtVMqjZqp_6qJJOp8' }}
+                    bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
                     defaultCenter={coordinates}
                     center={coordinates}
                     defaultZoom={ 14}
                     margin={[50, 50, 50, 50]}
-                    options={''}
+                    options={{
+                        disableDefaultUI: true,zoomControl:true,styles:mapStyles}}
                     onChange={(e)=>{
                         setCoordinates({ lat: e.center.lat, lng: e.center.lng });
                         setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
